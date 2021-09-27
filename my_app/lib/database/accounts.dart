2b8a95fa3b953ac95c41e53a1172d51416395f4f
Riri Cordero <41848305+Riricchan@users.noreply.github.com>
2021-09-27 15:43:20 +0800

    Added Dashboard

---
+++ my_app/lib/database/accounts.dart
@@ -0,0 +1,32 @@
+class Accounts {
+  String user;
+  String accNumber; //max 11 char
+  int balance;
+  int cardColor;
+
+  Accounts(this.user, this.accNumber, this.balance, this.cardColor);
+}
+
+List<Accounts> accounts = accountsData
+    .map(
+      (item) => Accounts(
+          item['user'] as String,
+          item['accNumber'] as String,
+          item['balance'] as int,
+          item['cardColor'] as int,
+    ),).toList();
+
+var accountsData = [
+  {
+    "user": "Lorem Ipsum",
+    "accNumber": "00023678234",
+    "balance": 15080.59,
+    "cardColor": 0xFF1E1E99
+  },
+  {
+    "user": "Lorem Ipsum",
+    "accNumber": "98675346875",
+    "balance": 200,
+    "cardColor": 0xFFFF70A3
+  }
+];
--- my_app/lib/home_screen.dart
+++ my_app/lib/home_screen.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/database/accounts.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Text("Dashboard goes here")
-      ],)
-    );
+        body: Container(
+            margin: EdgeInsets.only(top: 8),
+            child: ListView(
+              physics: ClampingScrollPhysics(),
+              children: [
+                // MENU BAR
+                Container(
+                  margin: EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.menu_rounded,
+                        color: Colors.blue,
+                        size: 50,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                // HEADER
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Welcome back,",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w500)),
+                        Text("Lorem Ipsum",
+                            style: TextStyle(
+                                color: Colors.blue[700],
+                                fontSize: 30,
+                                fontWeight: FontWeight.w900))
+                      ]),
+                ),
+                Container(
+                    height: 199,
+                    child: ListView.builder(
+                      padding: EdgeInsets.only(left: 16, right: 6),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: accounts.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 10),
+                          height: 199,
+                          width: 344,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color:Color(accounts[index].cardColor)),
+                        );
+                      }))
+              ],
+            )));
   }
 }
