fbd8fa5cd513cf10b8fea09f065b18552d879a40
naAlmanzor <neilalmanzor28@gmail.com>
2021-10-04 11:33:38 +0800

    Added under construction

---
+++ lib/under_construction_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UnderConstructionScreen extends StatelessWidget {
+  const UnderConstructionScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
