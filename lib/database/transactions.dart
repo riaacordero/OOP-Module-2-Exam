18dc382918b02306fe61dfeb567dd042004990b3
hrjugar <hernanjugar123@gmail.com>
2021-10-08 06:28:03 +0800

    Initialized transactions

---
+++ lib/database/transactions.dart
@@ -0,0 +1,23 @@
+import 'package:whizbank/database/accounts.dart';
+
+enum TransactionType {
+  TRANSFER, WITHDRAW, DEPOSIT
+}
+
+class Transaction {
+  Account accFrom;
+  String accTo;
+  double amount;
+  TransactionType type;
+  DateTime dateTime;
+
+  Transaction({
+    required this.accFrom, 
+    required this.accTo,
+    required this.amount,
+    required this.type,
+    required this.dateTime
+  });
+}
+
+List<Transaction> transactions = new List.empty();
