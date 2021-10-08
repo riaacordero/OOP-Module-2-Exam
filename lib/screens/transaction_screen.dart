3dac64bf329105b7cf1c612beff56774be6b3b13
Ria Cordero <riarcordero@gmail.com>
2021-10-08 10:32:27 +0800

    Added UI for empty transaction history screen

--- lib/screens/transaction_screen.dart
+++ lib/screens/transaction_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:whizbank/database/transactions.dart';
 
 class TransactionScreen extends StatelessWidget {
@@ -19,17 +20,40 @@ class TransactionScreen extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white
         ),
-        child: Scrollbar(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Padding(
-              padding: const EdgeInsets.all(18),
-              child: transactions.isEmpty 
-                ? Align(
-                  alignment: Alignment.center,
-                  child: Text("Hello World"),
-                )
-                : ListView.builder(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: transactions.isEmpty 
+            ? Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: 
+                  [Column(
+                    children: [
+                      SvgPicture.asset('assets/empty-transact.svg'),
+                      Padding(padding: const EdgeInsets.symmetric(vertical:15)),
+                      Text("Sike!",style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey),
+                        textAlign: TextAlign.center,),
+                      Padding(padding: const EdgeInsets.symmetric(vertical:5)),
+                      Text("Your transaction history is currently empty. It's either you have no friends or no money :(",style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey),
+                        textAlign: TextAlign.center,)
+                    ],
+                  ),
+                ],
+              ),
+            )
+            : Scrollbar(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: ListView.builder(
                   itemCount: transactions.length,
                   physics: ScrollPhysics(),
                   shrinkWrap: true,
@@ -47,11 +71,11 @@ class TransactionScreen extends StatelessWidget {
                         transactionType = "Deposit Money";
                         break;
                     }
-
+              
                     String transactionDateTime = currentTransaction.dateTime
                       .toString()
                       .substring(0, 16);
-
+              
                     return ExpansionTile(
                       title: Text(
                         "$transactionType \n" +
@@ -71,9 +95,9 @@ class TransactionScreen extends StatelessWidget {
                       ],
                     );
                   }
-                )
+                ),
+              ),
             )
-          )
         ),
       ),
     );
