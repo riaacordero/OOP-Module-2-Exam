5f9a6a37cdcdc917d135c4e42f7b0699e444cfdc
Ria Cordero <riarcordero@gmail.com>
2021-10-08 11:02:12 +0800

    Renamed file

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:whizbank/cards/account_card.dart';
 import 'package:whizbank/cards/action_card.dart';
 import 'package:whizbank/database/actions.dart';
-import 'package:whizbank/screens/transaction_screen.dart';
+import 'package:whizbank/screens/transaction_history_screen.dart';
 import 'package:whizbank/screens/under_construction_screen.dart';
 
 class HomeScreen extends StatelessWidget {
